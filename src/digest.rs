cb9bd8cef85a99d29c869165aa0f1ccff4a2cb5b
jayphbee <jayphbee@163.com>
2020-02-14 10:11:26 +0800

    add md5 support

--- src/digest.rs
+++ src/digest.rs
@@ -2,11 +2,14 @@
 * SHA加密算法
 */
 use ring::digest as rdigest;
+use rcrypto::md5::Md5;
+pub use rcrypto::digest::Digest;
 
 /**
 * SHA加密算法类型
 */
 pub enum DigestAlgorithm {
+	MD5,
 	SHA1,
 	SHA256,
 	SHA384,
@@ -21,6 +24,13 @@ pub enum DigestAlgorithm {
 */
 pub fn digest(alg: DigestAlgorithm, data: &[u8]) -> Vec<u8> {
 	match alg {
+		DigestAlgorithm::MD5 => {
+			let mut md5 = Md5::new();
+			md5.input(data);
+			let mut out = vec![0u8; 16];
+			md5.result(&mut out);
+			out
+		}
 		DigestAlgorithm::SHA1 => rdigest::digest(&rdigest::SHA1_FOR_LEGACY_USE_ONLY, data).as_ref().to_vec(),
 		DigestAlgorithm::SHA256 => rdigest::digest(&rdigest::SHA256, data).as_ref().to_vec(),
 		DigestAlgorithm::SHA384 => rdigest::digest(&rdigest::SHA384, data).as_ref().to_vec(),
@@ -28,16 +38,21 @@ pub fn digest(alg: DigestAlgorithm, data: &[u8]) -> Vec<u8> {
 	}
 }
 
-// #[cfg(test)]
-// mod tests {
-// 	use super::*;
-// 	use hex::FromHex;
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use hex::FromHex;
 
-// 	#[test]
-// 	fn test_sha1() {
-// 		let computed = digest(DigestAlgorithm::SHA1, b"abc");
-// 		let expected = Vec::from_hex("a9993e364706816aba3e25717850c26c9cd0d89d").unwrap();
-// 		assert_eq!(computed, expected);
-// 	}
-// 	// more tests here
-// }
+	#[test]
+	fn test_sha1() {
+		let computed = digest(DigestAlgorithm::SHA1, b"abc");
+		let expected = Vec::from_hex("a9993e364706816aba3e25717850c26c9cd0d89d").unwrap();
+		assert_eq!(computed, expected);
+	}
+	#[test]
+	fn test_md5() {
+		let computed = digest(DigestAlgorithm::MD5, b"abc");
+		let expected = Vec::from_hex("900150983CD24FB0D6963F7D28E17F72").unwrap();
+		assert_eq!(computed, expected);
+	}
+}
